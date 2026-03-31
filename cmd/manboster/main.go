56728c7e4840a856afa2279f3ba0aa157366304b
chihuo2104 <i@chihuo2104.dev>
2026-04-01 00:15:26 +0800

    feat(cmd): add missing things

---
+++ cmd/manboster/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/manboster/manboster/core/cli"
+	"github.com/manboster/manboster/core/config"
+)
+
+// Manboster: Your Personal Manbo Lobster!
+// Powered by chihuo2104 (c) 2026.
+// Last Update: 2026.3.31
+
+func main() {
+	config.Init()
+	cli.Init()
+}
