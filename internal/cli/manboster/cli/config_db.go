666191e20fb3a9df20813e51fb1a7e6c1ab19fa0
chihuo2104 <i@chihuo2104.dev>
2026-05-05 02:33:03 +0800

    feat(config): move db actions

--- internal/cli/manboster/cli/config_db.go
+++ internal/cli/manboster/cli/config_db.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/charmbracelet/huh"
@@ -99,7 +98,7 @@ func (s *databaseConfigService) runConfigDatabaseSessionSelection(ctx context.Co
 	err = huh.NewForm(
 		huh.NewGroup(
 			huh.NewSelect[databaseConfigSessionSelection]().Options(
-				huh.NewOption("Purge Session Data", databaseConfigSessionPurge),
+				huh.NewOption("Purge Unused Session Data (Save Space)", databaseConfigSessionPurge),
 				huh.NewOption("Select Sessions", databaseConfigSessionSelect),
 				huh.NewOption("Quit", databaseConfigSessionQuit),
 			).Value(&se).Description("What do you want to do?"),
@@ -117,12 +116,12 @@ func (s *databaseConfigService) runConfigDatabaseSessionSelection(ctx context.Co
 			helper.ClearScreen()
 			return nil
 		}
-		if helper.ContinueConfirm(ctx, fmt.Sprintf("Do you really want to purge %d sessions? YOUR ACTION IS IRREVERSIBLE!", purgeNum)) {
+		if helper.ContinueConfirm(ctx, fmt.Sprintf("Do you really want to DELETE %d unused sessions? YOUR ACTION IS IRREVERSIBLE!", purgeNum)) {
 			err := s.purgeConfigDatabaseSession(ctx)
 			if err != nil {
 				return err
 			}
-			color.Blue(fmt.Sprintf("Successfully purged %d sessions!", purgeNum))
+			color.Blue(fmt.Sprintf("Successfully deleted %d unused sessions!", purgeNum))
 			time.Sleep(500 * time.Millisecond)
 			helper.ClearScreen()
 		}
@@ -135,77 +134,4 @@ func (s *databaseConfigService) runConfigDatabaseSessionSelection(ctx context.Co
 	default:
 		return fmt.Errorf("unexpected database session selection: %s", se)
 	}
-}
-
-func (s *databaseConfigService) printConfigDatabaseSessionList(ctx context.Context) error {
-	// string is sessionID
-	chatsMap := make(map[string][]types.Chat)
-
-	sessions, err := s.repo.GetSessions(ctx)
-	if err != nil {
-		return err
-	}
-	s.sessions = sessions
-
-	chats, err := s.repo.GetAllChats(ctx)
-	if err != nil {
-		return err
-	}
-	s.chats = chats
-
-	for _, c := range chats {
-		cm, avail := chatsMap[c.SessionID]
-		if !avail {
-			cm = []types.Chat{c}
-			chatsMap[c.SessionID] = cm
-			continue
-		}
-		cm = append(cm, c)
-		chatsMap[c.SessionID] = cm
-	}
-	s.chatsMap = chatsMap
-
-	var outputMsg strings.Builder
-	for _, sess := range s.sessions {
-		outputMsg.WriteString(fmt.Sprintf("%d) `%s`, used `%s:%s` created at %s, updated at %s.\n", sess.ID, sess.SessionID, sess.LLMProvider, sess.LLMProviderModel, sess.CreatedAt.Format("2006-01-02 15:04:05"), sess.UpdatedAt.Format("2006-01-02 15:04:05")))
-		cm, avail := chatsMap[sess.SessionID]
-		if avail {
-			outputMsg.WriteString(fmt.Sprintf("Bind %d chats: ", len(cm)))
-			for _, c := range cm {
-				outputMsg.WriteString(fmt.Sprintf("%s:%s ", c.ChatProvider, c.ChatID))
-			}
-			outputMsg.WriteString("\n")
-		}
-	}
-
-	outputMsg.WriteString(fmt.Sprintf("%d sessions loaded, %d sessions can be purged.", len(s.sessions), len(sessions)-len(chatsMap)))
-
-	helper.DisplayText(outputMsg.String())
-	return nil
-}
-
-func (s *databaseConfigService) purgeConfigDatabaseSession(ctx context.Context) error {
-	for _, sess := range s.sessions {
-		cm, avail := s.chatsMap[sess.SessionID]
-		if !avail {
-			err := s.repo.DeleteSession(ctx, sess.SessionID)
-			if err != nil {
-				color.Yellow(fmt.Sprintf("[Manboster Client] Error purging session %s: %q", sess.SessionID, err))
-				continue
-			}
-			err = s.repo.DeleteChatData(ctx, sess.SessionID)
-			if err != nil {
-				color.Yellow(fmt.Sprintf("[Manboster Client] Error purging session %s: %q", sess.SessionID, err))
-				continue
-			}
-			for _, c := range cm {
-				err := s.repo.DeleteChat(ctx, c.ChatID, c.ChatProvider)
-				if err != nil {
-					color.Yellow(fmt.Sprintf("[Manboster Client] Error purging session %s: %q", sess.SessionID, err))
-					continue
-				}
-			}
-		}
-	}
-	return nil
 }
