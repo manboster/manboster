4479bfbbf294994bd704467e96c755e5c2a5cf33
chihuo2104 <i@chihuo2104.dev>
2026-05-17 23:59:47 +0800

    feat(spec): add cli provider

---
+++ spec/cli/provider.go
@@ -0,0 +1,15 @@
+package cli
+
+type Option[T any] struct {
+	Key      string
+	Value    T
+	Selected bool
+}
+
+// Provider defines cli provider's functions
+type Provider interface {
+	Select(title string, description string, options []Option[any], validate func(option Option[any]) bool) (Option[any], error)
+	MultiSelect(title string, description string, options []Option[any], validate func(options []Option[any]) bool) ([]Option[any], error)
+	Input(title string, description string, validate func(input any) bool) (any, error)
+	Prompt(title string, description string, t string, f string) (bool, error)
+}
