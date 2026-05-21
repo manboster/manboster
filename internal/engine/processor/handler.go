ee2730449d845ddc45282768567e9f3d9c146db5
chihuo2104 <i@chihuo2104.dev>
2026-05-21 23:29:10 +0800

    fix(processor): error handling logic

--- internal/engine/processor/handler.go
+++ internal/engine/processor/handler.go
@@ -17,39 +17,44 @@ func (s *Service) Process(ctx context.Context, instance chat.Provider, msg *chat
 	// before receiving messages, we should check users' identity.
 	// get user information
 	uType := s.safeguardService.UserType(ctx, instance.Name(), msg.UserID)
-
-	if !s.safeguardService.IsAdmin(uType) && msg.ChatType == chat.ChatsPersonal {
-		color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to talk with your lobster in person!"))
-		msg.MessageType = chat.MessageUnknown
+	sessionId := ""
+	if msg.MessageType&(chat.MessageSelectionCallback|chat.MessageSelection|chat.MessageCommand) == 0 {
+		if !s.safeguardService.IsAdmin(uType) && msg.ChatType == chat.ChatsPersonal {
+			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to talk with your lobster in person!"))
+			msg.MessageType = chat.MessageUnknown
+			resultProcess = ProcessHandle
+		}
+	} else if msg.MessageType == chat.MessageSelectionCallback || msg.MessageType == chat.MessageCommand {
 		resultProcess = ProcessHandle
-	}
+	} else {
+		var err error
+		// get message types
+		sessionId, err = s.sessionService.LoadChatSession(ctx, instance, msg, s.safeguardService.IsAdmin(uType))
+		// if you're not an administrator, you can not create a new session
+		if errors.Is(err, ErrAccessDenied) {
+			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to start a new chat!"))
+			msg.MessageType = chat.MessageUnknown
+			resultProcess = ProcessHandle
+		}
+		if err != nil {
+			color.Red(fmt.Sprintf("[Manboster Processor] We encountered an error while loading sessionId, error: %q", err))
+			return err
+		}
 
-	// get message types
-	sessionId, err := s.sessionService.LoadChatSession(ctx, instance, msg, s.safeguardService.IsAdmin(uType))
-	// if you're not an administrator, you can not create a new session
-	if errors.Is(err, ErrAccessDenied) {
-		color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to start a new chat!"))
-		msg.MessageType = chat.MessageUnknown
-		resultProcess = ProcessHandle
-	}
-	if err != nil {
-		color.Red(fmt.Sprintf("[Manboster Processor] We encountered an error while loading sessionId, error: %q", err))
-		return err
-	}
+		if s.onboard != nil && !s.onboard.Active() {
+			msg.MessageType = chat.MessageStart
+			resultProcess = ProcessHandle
+		}
 
-	if s.onboard != nil && !s.onboard.Active() {
-		msg.MessageType = chat.MessageStart
-		resultProcess = ProcessHandle
-	}
-
-	if msg.ChatType == chat.ChatsGroup {
-		resultProcess = ProcessDrop
-		if msg.MessageType&chat.MessageText != 0 {
-			if msg.Text != nil && strings.Contains(msg.Text.Text, "[[!@{Assistant}]]") {
-				resultProcess = ProcessHandle
-			}
-			if msg.Reply != nil && msg.Reply.Username == "Assistant" {
-				resultProcess = ProcessHandle
+		if msg.ChatType == chat.ChatsGroup {
+			resultProcess = ProcessDrop
+			if msg.MessageType&chat.MessageText != 0 {
+				if msg.Text != nil && strings.Contains(msg.Text.Text, "[[!@{Assistant}]]") {
+					resultProcess = ProcessHandle
+				}
+				if msg.Reply != nil && msg.Reply.Username == "Assistant" {
+					resultProcess = ProcessHandle
+				}
 			}
 		}
 	}
