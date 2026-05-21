5923467668b6e20c676749d99ca7c7e6de7caa75
chihuo2104 <i@chihuo2104.dev>
2026-05-21 17:11:17 +0800

    feat(engine): add quickchat method in gateway

---
+++ internal/engine/gateway/llm_quick.go
@@ -0,0 +1,37 @@
+package gateway
+
+import (
+	"context"
+
+	"github.com/manboster/manboster/spec/llm"
+)
+
+func (s *Service) LLMQuickChat(ctx context.Context, currentProvider llm.Provider, currentModel llm.Model, system string, user string) (*llm.Event, error) {
+	msgList := []llm.Message{
+		{
+			Type: llm.MessageText,
+			Role: llm.RoleSystem,
+			Parts: []llm.MessageParts{
+				{
+					PartsType: llm.MessagePartsText,
+					Text: &llm.MessageTextPayload{
+						Text: system,
+					},
+				},
+			},
+		},
+		{
+			Type: llm.MessageText,
+			Role: llm.RoleUser,
+			Parts: []llm.MessageParts{
+				{
+					PartsType: llm.MessagePartsText,
+					Text: &llm.MessageTextPayload{
+						Text: user,
+					},
+				},
+			},
+		},
+	}
+	return s.LLMChat(ctx, currentProvider, currentModel, msgList)
+}
