fed7bde9488579490b7b67635ac52dcb52041431
chihuo2104 <i@chihuo2104.dev>
2026-05-22 00:04:06 +0800

    fix(processor): handling logic error

--- internal/engine/processor/handler.go
+++ internal/engine/processor/handler.go
@@ -18,55 +18,48 @@ func (s *Service) Process(ctx context.Context, instance chat.Provider, msg *chat
 	// get user information
 	resultProcess = ProcessDrop
 	uType := s.safeguardService.UserType(ctx, instance.Name(), msg.UserID)
-	sessionId := ""
-	if msg.MessageType&(chat.MessageSelectionCallback|chat.MessageSelection|chat.MessageCommand) == 0 {
-		if !s.safeguardService.IsAdmin(uType) && msg.ChatType == chat.ChatsPersonal {
-			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to talk with your lobster in person!"))
-			msg.MessageType = chat.MessageUnknown
-			resultProcess = ProcessHandle
-		}
-	} else if msg.MessageType == chat.MessageSelectionCallback || msg.MessageType == chat.MessageCommand {
+	if msg.MessageType&(chat.MessageSelectionCallback|chat.MessageSelection|chat.MessageCommand) == 0 && msg.ChatType == chat.ChatsPersonal {
 		resultProcess = ProcessHandle
-	} else {
-		var err error
-		// get message types
-		sessionId, err = s.sessionService.LoadChatSession(ctx, instance, msg, s.safeguardService.IsAdmin(uType))
-		// if you're not an administrator, you can not create a new session
-		if errors.Is(err, ErrAccessDenied) {
-			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to start a new chat!"))
-			msg.MessageType = chat.MessageUnknown
-			resultProcess = ProcessHandle
-		}
-		if err != nil {
-			color.Red(fmt.Sprintf("[Manboster Processor] We encountered an error while loading sessionId, error: %q", err))
-			return err
-		}
-
 		if s.onboard != nil && !s.onboard.Active() {
 			msg.MessageType = chat.MessageStart
-			resultProcess = ProcessHandle
 		}
 
-		if msg.ChatType == chat.ChatsGroup {
-			resultProcess = ProcessDrop
-			if msg.MessageType&chat.MessageText != 0 {
-				if msg.Text != nil && strings.Contains(msg.Text.Text, "[[!@{Assistant}]]") {
-					resultProcess = ProcessHandle
-				}
-				if msg.Reply != nil && msg.Reply.Username == "Assistant" {
-					resultProcess = ProcessHandle
-				}
+		if !s.safeguardService.IsAdmin(uType) {
+			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to talk with your lobster in person!"))
+			msg.MessageType = chat.MessageUnknown
+		}
+
+	} else if msg.MessageType == chat.MessageSelectionCallback || msg.MessageType == chat.MessageCommand {
+		resultProcess = ProcessHandle
+	} else if msg.ChatType == chat.ChatsGroup {
+		resultProcess = ProcessDrop
+		if msg.MessageType&chat.MessageText != 0 {
+			if msg.Text != nil && strings.Contains(msg.Text.Text, "[[!@{Assistant}]]") {
+				resultProcess = ProcessHandle
+			}
+			if msg.Reply != nil && msg.Reply.Username == "Assistant" {
+				resultProcess = ProcessHandle
 			}
 		}
 	}
 
 	switch resultProcess {
 	case ProcessHandle:
+		// get message types
+		sessionId, err := s.sessionService.LoadChatSession(ctx, instance, msg, s.safeguardService.IsAdmin(uType))
+		// if you're not an administrator, you can not create a new session
+		if errors.Is(err, ErrAccessDenied) {
+			color.Yellow(fmt.Sprintf("[Manboster Processor] We detected an unknown user wants to start a new chat!"))
+			msg.MessageType = chat.MessageUnknown
+		} else if err != nil {
+			color.Red(fmt.Sprintf("[Manboster Processor] We encountered an error while loading sessionId, error: %q", err))
+			msg.MessageType = chat.MessageUnknown
+		}
 		return s.engine.Distribute(ctx, instance, msg, sessionId)
-	case ProcessDrop:
-		return nil
-	case ProcessConsider:
-		return nil // TODO: manboster active mode
+	//case ProcessDrop:
+	//	return nil
+	//case ProcessConsider:
+	//	return nil // TODO: manboster active mode
 	default:
 		return nil
 	}
